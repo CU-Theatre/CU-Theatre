cdc7c03592ce0d9d766e196e1b87a3b3e683c68c
artem <artemsa223@gmail.com>
2024-08-03 23:13:23 +0100

    added user-profile-controller + refactored user model( added tavle emergency contact)

---
+++ backend/src/main/java/cu/theater/backend/model/EmergencyContact.java
@@ -0,0 +1,38 @@
+package cu.theater.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "emergency_contact")
+public class EmergencyContact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, name = "phone_number")
+    private String phoneNumber;
+
+    @Column(nullable = false, name = "relation")
+    private String relation;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
