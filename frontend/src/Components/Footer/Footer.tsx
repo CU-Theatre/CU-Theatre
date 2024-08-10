78a5e5b73a6fb592a9d9249bc96d95ec36c908bb
Valentyn Sobol <valiksobol14@gmail.com>
2024-08-10 23:23:09 +0300

    add second section, delete css modules, add index.ts

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import style from "./Footer.module.scss";
-import container from "../../styles/container.module.scss";
+import './Footer.scss';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className={style.footer}>
-      <div className={container.container}>
-        <div className={style.footer__content}>
-          <div className={style.footer__location}>
-            <h3 className={style.footer__title}>Location</h3>
+    <footer className='footer'>
+      <div className='footer__container'>
+        <div className='footer__content'>
+          <div className='footer__location'>
+            <h3 className='footer__title'>Location</h3>
             <a
-              className={style.footer__link}
+              className='footer__link'
               href="https://maps.app.goo.gl/8xqPXXouaUgFtgxTA"
               target="_blank"
               rel="noopener noreferrer"
@@ -19,30 +18,30 @@ export const Footer: React.FC = () => {
             </a>
           </div>
 
-          <div className={style.footer__hours}>
-            <h3 className={style.footer__title}>Hours</h3>
-            <ul className={style.footer__list}>{/* add work hours */}</ul>
+          <div className='footer__hours'>
+            <h3 className='footer__title'>Hours</h3>
+            <ul className='footer__list'>{/* add work hours */}</ul>
           </div>
 
-          <div className={style.footer__contacts}>
-            <h3 className={style.footer__title}>Contact</h3>
-            <ul className={style.footer__list}>
-              <li className={style.footer__item}>
+          <div className='footer__contacts'>
+            <h3 className='footer__title'>Contact</h3>
+            <ul className='footer__list'>
+              <li className='footer__item'>
                 <a
                   href="mailto:"
                   target="_blank"
                   rel="noreferrer"
-                  className={style.footer__link}
+                  className='footer__link'
                 >
                   <div>Email</div>
                 </a>
               </li>
-              <li className={style.footer__item}>
+              <li className='footer__item'>
                 <a
                   href="tel:+"
                   target="_blank"
                   rel="noreferrer"
-                  className={style.footer__link}
+                  className='footer__link'
                 >
                   <div>Tel</div>
                 </a>
@@ -50,8 +49,8 @@ export const Footer: React.FC = () => {
             </ul>
           </div>
 
-          <div className={style.footer__sadMask} />
-          <div className={style.footer__smilingMask} />
+          <div className='footer__sadMask' />
+          <div className='footer__smilingMask' />
         </div>
       </div>
     </footer>
---
+++ frontend/src/Page/signButton/SignButton.tsx
@@ -0,0 +1,8 @@
+import  React from "react";
+import './SignButton.scss';
+
+export const SignButton: React.FC = () => {
+  return (
+    <button className="sign-button">Sign for a course</button>
+  );
+};
