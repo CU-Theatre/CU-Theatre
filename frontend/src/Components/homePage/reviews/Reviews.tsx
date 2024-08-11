139cd30b7184bcf0d9da5440e17737ed51daea74
Valentyn Sobol <valiksobol14@gmail.com>
2024-08-11 19:30:39 +0300

    add final homesection, add swiper component

--- frontend/src/Components/homePage/reviews/Reviews.tsx
+++ frontend/src/Components/homePage/reviews/Reviews.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import 'video-react/dist/video-react.css';
+import './Reviews.scss';
+import { ReviewsSwiper } from "./swiper";
+import { useFadeIn } from "../../../hooks/useFadeIn";
 
-export const Review: React.FC = () => {
+export const Reviews: React.FC = () => {
+  const [ref, isVisible] = useFadeIn();
+
   return (
-    <div></div>
+    <div className="home__reviews reviews">
+      <div ref={ref} className={`reviews__container ${isVisible ? 'block-visible' : ''}`}>
+        <h2 className="reviews__title title">Heartfelt Words from Our Audience</h2>
+        <div className="reviews__content">
+          <ReviewsSwiper />
+        </div>
+      </div>
+    </div>
   );
 };
