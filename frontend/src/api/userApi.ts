b407db9f9f00c5dc77fe67787cb1297e43df8c81
Vasil Liakhivskyi <vazilx@gmail.com>
2024-08-26 10:56:12 +0200

    rewrite cabinet form to react-hook-forms

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -4,3 +4,7 @@ import { client } from "../utils/fetchClient";
 export const getCurrentUser = (token: string) => {
   return client.get<User>("/user/current-user", token);
 };
+
+export const updateUser = (id: number) => {
+  
+}
