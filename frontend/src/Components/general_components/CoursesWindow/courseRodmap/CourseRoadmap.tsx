fe6c3c719a0c295723f0bfac8fccd6918b98061d
Vasil Liakhivskyi <vazilx@gmail.com>
2024-09-17 08:10:01 +0200

    refactoring roadmap

--- frontend/src/Components/general_components/CoursesWindow/courseRodmap/CourseRoadmap.tsx
+++ frontend/src/Components/general_components/CoursesWindow/courseRodmap/CourseRoadmap.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CourseType } from "../../../../types/CourseType";
 import "./CourseRodmap.scss";
 import classNames from "classnames";
@@ -8,9 +8,19 @@ interface Props {
 }
 
 export const CourseRoadmap: React.FC<Props> = ({ course }) => {
+  const [sortedRoadmap, setSortedRoadmap] = useState(course.roadmap);
+
+  useEffect(() => {
+    const sortedRoadmap = [...course.roadmap];
+
+    sortedRoadmap.sort((point1, point2) => point1.id - point2.id);
+
+    setSortedRoadmap(sortedRoadmap);
+  }, [course.roadmap]);
+
   return (
     <div className="course-window__roadmap roadmap">
-      {course.roadmap.map((period, index) => (
+      {sortedRoadmap.map((period, index) => (
         <div
           className={classNames(`roadmap__block roadmap__block--${index}`)}
           key={period.id}
